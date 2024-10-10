bb4c8d1c78b031979f5ffb3a29058d0e18a02daa
4kitsw10 <4kitsw10@solent.ac.uk>
2024-10-10 10:54:36 +0100

    expert calculate_unique_conditions made private

--- expert.py
+++ expert.py
@@ -2,16 +2,16 @@ from rule import Rule
 
 class Expert:
     def __init__(self):
-        self.rules = []
-        self.facts = []
+        self.__rules = []
+        self.__facts = []
 
     def add_rule(self, conditions, conclusion):
-        self.rules.append(Rule(conditions, conclusion))
+        self.__rules.append(Rule(conditions, conclusion))
 
     def investigate(self):
-        for unique_condition in self.calculate_unique_conditions():
-            for rule in self.rules:
-                if all(fact in self.facts for fact in rule.conditions):
+        for unique_condition in self.__calculate_unique_conditions():
+            for rule in self.__rules:
+                if all(fact in self.__facts for fact in rule.conditions):
                     if not rule.incorrect:
                         if input(f"A: {rule.conclusion}? (yes/no): ") == 'yes':
                             return
@@ -20,12 +20,12 @@ class Expert:
                             rule.incorrect = True
 
             if input(f"Q: {unique_condition}? (yes/no): ") == 'yes':
-                self.facts.append(unique_condition)
+                self.__facts.append(unique_condition)
 
-    def calculate_unique_conditions(self):
+    def __calculate_unique_conditions(self):
         output = []
 
-        for rule in self.rules:
+        for rule in self.__rules:
             for condition in rule.conditions:
                 if condition not in output:
                     output.append(condition)
@@ -33,4 +33,4 @@ class Expert:
         return output
 
     def explain_conclusion(self):
-        return f"facts: {self.facts}"
+        return f"facts: {self.__facts}"
